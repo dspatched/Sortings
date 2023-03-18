5cc22b91b21779f656c278313f267c728cf80b99
DudkinIlya <dudkin.iluha@gmail.com>
2023-03-18 12:10:09 +0300

    4th commit

--- src/com/dspatched/TreeWalker.java
+++ src/com/dspatched/TreeWalker.java
@@ -1,6 +1,7 @@
 package com.dspatched;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class TreeWalker {
@@ -8,78 +9,95 @@ public class TreeWalker {
     TreeWalker(Node node) {
         currentNode = node;
     }
-
     private Node currentNode;
-
     public void setCurrentNode(Node currentNode) {
         this.currentNode = currentNode;
     }
 
     public static void main(String[] args) {
-        Node root = populateTree();
+        Node root = populateTree(List.of(0,1,2,3,4,5));
         TreeWalker treeWalker = new TreeWalker(root);
-        System.out.println("BREADTH FIRST:");
+        System.out.print("BREADTH FIRST: ");
         treeWalker.traverseBreadthFirst();
 
         treeWalker.setCurrentNode(root);
         System.out.println();
-        System.out.println("DEPTH FIRST:");
+        System.out.print("DEPTH FIRST: ");
         treeWalker.traverseDepthFirst();
     }
 
-    private static Node populateTree() {
-        int cnt = 0;
-        System.out.print(cnt);
-        Node node = new Node(cnt++);
-        System.out.print(cnt);
-        node.left = new Node(cnt++);
-        System.out.print(cnt);
-        node.right = new Node(cnt++);
-        System.out.print(cnt);
-        node.left.left = new Node(cnt++);
-        System.out.print(cnt);
-        node.left.right = new Node(cnt++);
-        System.out.print(cnt);
-        node.right.left = new Node(cnt++);
-        System.out.println();
-        return node;
+    private static Node populateTree(List<Integer> list) {
+        Queue<Node> queue = new LinkedList<>();
+        int i = 0;
+        Node root = new Node(list.get(i++));
+        queue.add(root);
+
+        while (!queue.isEmpty() && i < list.size()) {
+            Node node = queue.poll();
+
+            Integer left = list.get(i++);
+            if (left != null) {
+                node.left = new Node(left);
+                queue.add(node.left);
+            }
+
+            if (i < list.size()) {
+                Integer right = list.get(i++);
+                if (right != null) {
+                    node.right = new Node(right);
+                    queue.add(node.right);
+                }
+            }
+        }
+        return root;
     }
 
     public void traverseBreadthFirst() {
-        Queue<Node> queue = new LinkedList<>();
         if (currentNode == null) return;
-        System.out.print(currentNode.id);
+        Queue<Node> queue = new LinkedList<>();
         queue.offer(currentNode);
         while (!queue.isEmpty()) {
             currentNode = queue.poll();
+            System.out.print(currentNode.id + " ");
             if (currentNode.left != null) {
                 queue.offer(currentNode.left);
-                System.out.print(currentNode.left.id);
             }
             if (currentNode.right != null) {
                 queue.offer(currentNode.right);
-                System.out.print(currentNode.right.id);
             }
         }
     }
 
     public void traverseDepthFirst() {
-        LinkedList<Node> stack = new LinkedList<>();
         if (currentNode == null) return;
+        LinkedList<Node> stack = new LinkedList<>();
         stack.push(currentNode);
+
         while (!stack.isEmpty()) {
             Node node = stack.pop();
-            while (node != null) {
-                System.out.print(node.id);
-                if (node.left != null) {
-                    if (node.right != null) stack.push(node.right);
-                    node = node.left;
-                } else {
-                    node = node.right;
-                }
+            System.out.print(node.id + " ");
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
             }
         }
-        System.out.println();
+    }
+
+    public void invert() {
+        invert(currentNode);
+    }
+
+    private void invert(Node node) {
+        if (node == null) return;
+
+        Node tmp = node.left;
+        node.left = node.right;
+        node.right = tmp;
+
+        invert(node.left);
+        invert(node.right);
     }
 
 }
