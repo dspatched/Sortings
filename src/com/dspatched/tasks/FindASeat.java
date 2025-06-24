04b7e93bc3346128cc5d3b5a8a1807d0f38eef5d
DudkinIlya <dudkin.iluha@gmail.com>
2025-06-24 14:07:07 +0300

    leetcode

--- src/com/dspatched/tasks/FindASeat.java
+++ src/com/dspatched/tasks/FindASeat.java
@@ -7,6 +7,7 @@ public class FindASeat {
         //int seat = fs.findSeat(new int[]{0,1,1,0,0});
         int seat = fs.findSeat(new int[]{0,1,0,0,1,0,0,0,0,0,1});
         System.out.println("EMPTY SEAT NUMBER WHICH IS FARTHEST FROM OTHERS: " + seat);
+        System.out.println(maxDistToClosest(new boolean[]{true,false,false,false,false,false,false,true,false,false,false, false,false}));
     }
 
     private int findSeat(int[] seats) {
@@ -71,5 +72,61 @@ public class FindASeat {
         }
 
         return -1;
+    }
+
+    public static int maxDistToClosest(boolean[] seats) {
+        int len = seats.length;
+        boolean leftEdge = true;
+        boolean rightEdge = false;
+
+        int curEmpty = 0;
+        int maxEmpty = 0;
+        int leftEdgeMax = 0;
+        int rightEdgeMax = 0;
+        int result = 0;
+
+        for (int i = 0; i < len; i++) {
+            if (i == len - 1) {
+                rightEdge = true;
+            }
+
+            boolean cur = seats[i];
+            if (!cur && leftEdge == true) {
+                curEmpty++;
+                leftEdgeMax++;
+                if (maxEmpty < curEmpty) {
+                    maxEmpty = curEmpty;
+                }
+            } else if (!cur && leftEdge == false) {
+                curEmpty++;
+                if (rightEdge) {
+                    rightEdgeMax = curEmpty;
+                }
+                if (maxEmpty < curEmpty) {
+                    if (curEmpty % 2 == 0) {
+                        result = curEmpty / 2;
+                    } else {
+                        result = curEmpty / 2 + 1;
+                    }
+                    int biggerEdgeCase = Math.max(leftEdgeMax, rightEdgeMax);
+                    result = result < biggerEdgeCase ? biggerEdgeCase : result;
+                }
+            } else if (cur) {
+                leftEdge = false;
+                rightEdge = false;
+                if (maxEmpty < curEmpty) {
+                    if (curEmpty % 2 == 0) {
+                        result = curEmpty / 2;
+                    } else {
+                        result = curEmpty / 2 + 1;
+                    }
+                }
+                int biggerEdgeCase = Math.max(leftEdgeMax, rightEdgeMax);
+                result = result < biggerEdgeCase ? biggerEdgeCase : result;
+                curEmpty = 0;
+            }
+        }
+
+        return result;
     }
 }
